51c1ba27d36837a42c2ec81fbc6ca9bfe7cbb5e0
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2020-06-28 02:01:58 +0300

    creates README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Static Server
+Serving a static page using Node.js
+
+The sample code demonstrates the following:
+- Getting up and running with Node.js.
+- Using `fs` module.
+- Using `http` module.
+- Serving a static HTML page
--- index.js
+++ index.js
@@ -5,21 +5,25 @@ const PORT = 3000;
 const PUBLIC_PATH = __dirname + '/public';
 
 const server = http.createServer( (req, res) => {
+    // serving only the index page
     if (req.url === "/" || req.url === '/index') {
+        // reading HTML file asynchronously
         fs.readFile(PUBLIC_PATH + '/index.html', (err, data) => {
             if (err) {
                 console.log('Error', err.message);
-    
+                
                 res.statusCode = 404;
                 res.end('Something went wrong');
                 return;
             }
-    
+            
             res.statusCode = 200;
+            // respond with index.html
             res.end(data);
             return;
         })
     } else {
+        // if user requested page other than index.html
         res.statusCode = 404;
         res.end('Page does not exist');
     }
